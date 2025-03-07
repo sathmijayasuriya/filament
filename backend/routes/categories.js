21c444e909ee84129f40d18dc0d3612e196445ec
sathmijayasuriya <sathmijayasuriya@gmail.com>
2025-03-07 10:26:23 +0530

    Updated category api endpoints

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -5,7 +5,7 @@ const categoriesController = require('../controllers/categoriesController');
 router.get('/', categoriesController.getAllCategories);
 router.get('/names', categoriesController.getAllCategoryNames);
 router.get('/:slug', categoriesController.getCategoryBySlug);
-router.post('/', categoriesController.createCategory);
+router.post('/create', categoriesController.createCategory);
 router.put('/:slug', categoriesController.updateCategory);
 router.delete('/:slug', categoriesController.deleteCategory);
 
