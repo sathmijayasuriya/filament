1562f3f589d53ec48998a9d32e664985f711d8bb
sathmijayasuriya <sathmijayasuriya@gmail.com>
2025-05-04 17:15:10 +0530

    feat: Implement full links management with CRUD

---
+++ frontend/src/components/links/LinksTableHeader.jsx
@@ -0,0 +1,32 @@
+import { Button } from "@/components/ui/button";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Link } from "react-router-dom";
+
+export default function LinksTableHeader() {
+  return (
+    <div className="p-6">
+    <Breadcrumb>
+      <BreadcrumbItem>
+        <BreadcrumbLink href="/links">Links</BreadcrumbLink>
+      </BreadcrumbItem>
+      <BreadcrumbSeparator />
+      <BreadcrumbItem>
+        <BreadcrumbLink href="#">List</BreadcrumbLink>
+      </BreadcrumbItem>
+    </Breadcrumb>
+    <div className="flex justify-between items-center mt-4">
+      <h2 className="text-3xl text-black font-bold">Links</h2>
+      <Link to="/links/create">
+        <Button className="bg-orange-400 hover:bg-orange-500 text-white">
+          New Link
+        </Button>
+      </Link>
+    </div>
+  </div>
+  )
+}
