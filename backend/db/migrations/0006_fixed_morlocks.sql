62687613f910a2765d6967b0a24010d569ff795d
sathmijayasuriya <sathmijayasuriya@gmail.com>
2025-05-06 19:37:39 +0530

    feat: Implement full links management with CRUD in backend

---
+++ backend/db/migrations/0006_fixed_morlocks.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `links` (
+	`id` int AUTO_INCREMENT NOT NULL,
+	`title` varchar(255) NOT NULL,
+	`color` varchar(7) NOT NULL,
+	`description` text,
+	`url` varchar(2048) NOT NULL,
+	`image` varchar(255),
+	`created_at` timestamp DEFAULT (now()),
+	`updated_at` timestamp ON UPDATE CURRENT_TIMESTAMP,
+	CONSTRAINT `links_id` PRIMARY KEY(`id`)
+);
